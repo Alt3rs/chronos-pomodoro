620dd7df3f59fc7bfea5afc41dac6889fd63b25d
Pedro Rios <PRios731@gmail.com>
2025-04-25 17:11:35 -0300

    Creating a component for container.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import { Heading } from './components/Heading';
-
 import './style/theme.css';
 import './style/global.css';
-import { TimerIcon } from 'lucide-react';
+
+import { Container } from './components/Container';
+import { Heading } from './components/Heading';
 
 export function App() {
   return (
     <>
-      <Heading>
-        Olá mundo
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet ab debitis
-        quod accusamus molestias, similique voluptas assumenda fugit ea nesciunt
-        sunt accusantium, maiores odit voluptatem. Tempore cum soluta nesciunt
-        alias.
-      </p>
+      <Container>
+        <Heading>Logo</Heading>
+      </Container>
+
+      <Container>
+        <Heading>Menu</Heading>
+      </Container>
     </>
   );
 }
