d1ca9a3a9f01826db07a641fe455467445ca5236
Pedro Rios <PRios731@gmail.com>
2025-04-25 14:19:37 -0300

    Finalizing the Heading component and typing TypeScript for children (ReactNode)

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import { Heading } from './components/Heading';
 
 import './style/theme.css';
 import './style/global.css';
+import { TimerIcon } from 'lucide-react';
 
 export function App() {
   return (
     <>
-      <Heading attr={123} attr2='string'>
+      <Heading>
         Olá mundo
+        <button>
+          <TimerIcon />
+        </button>
       </Heading>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet ab debitis
