a8315ad0fd4895baf1a03f2ad4c537e1044f91f9
Pedro Rios <PRios731@gmail.com>
2025-05-05 12:55:11 -0300

    Creating a Singleton to manage the worker (TimerWorkerManager).

--- src/Context/TaskContext/TaskContextProvider.tsx
+++ src/Context/TaskContext/TaskContextProvider.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useReducer } from 'react';
 import { initialTaskState } from './initialTaskState';
 import { TaskContext } from './TaskContext';
-import { taskReducer } from './TaskReducer';
+import { taskReducer } from './taskReducer';
 
 type TaskContextProviderProps = {
   children: React.ReactNode;
