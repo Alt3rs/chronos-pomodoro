83f641a30cae653c34da18f5ea19cba62d12265e
Pedro Rios <PRios731@gmail.com>
2025-05-06 10:38:04 -0300

    React Toastify - adding messages to the user.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import './style/theme.css';
 import './style/global.css';
-
 import { Home } from './Pages/Home';
 import { TaskContextProvider } from './Context/TaskContext/TaskContextProvider';
+import { MessagesContainer } from './components/MessagesContainer';
 
 export function App() {
   return (
     <TaskContextProvider>
-      <Home />
+      <MessagesContainer>
+        <Home />
+      </MessagesContainer>
     </TaskContextProvider>
   );
 }
