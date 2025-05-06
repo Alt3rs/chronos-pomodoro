4862717466630f598526fa67869911f3149a6458
Pedro Rios <PRios731@gmail.com>
2025-05-06 13:16:53 -0300

    React Router - Starting the application routing.

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,21 @@ import './style/global.css';
 import { Home } from './Pages/Home';
 import { TaskContextProvider } from './Context/TaskContext/TaskContextProvider';
 import { MessagesContainer } from './components/MessagesContainer';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import { NotFound } from './Pages/NotFound/indes';
+import { AboutPomodoro } from './Pages/AboutPomodoro/indes';
 
 export function App() {
   return (
     <TaskContextProvider>
       <MessagesContainer>
-        <Home />
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home />}></Route>
+            <Route path='/about-pomodoro/' element={<AboutPomodoro />}></Route>
+            <Route path='*' element={<NotFound />}></Route>
+          </Routes>
+        </BrowserRouter>
       </MessagesContainer>
     </TaskContextProvider>
   );
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,10 +1,13 @@
+import { Link } from 'react-router';
 import styles from './styles.module.css';
 
 export function Footer() {
   return (
     <footer className={styles.footer}>
-      <a href=''>Entenda como funciona a técnica pomodoro</a>
-      <a href=''>Chronos Pomodoro &copy;</a>
+      <Link to='/about-pomodoro/'>
+        Entenda como funciona a técnica pomodoro
+      </Link>
+      <Link to='/'>Chronos Pomodoro &copy;</Link>
     </footer>
   );
 }
