629070cf8938cec01f2639c29b8e4229b7cf7d20
Pedro Rios <PRios731@gmail.com>
2025-05-07 09:38:39 -0300

    Filling the history with real state data.

--- src/Pages/History/index.tsx
+++ src/Pages/History/index.tsx
@@ -5,8 +5,11 @@ import { Heading } from '../../components/Heading';
 import { MainTemplate } from '../../Templates/MainTemplate';
 
 import styles from './styles.module.css';
+import { useTaskContext } from '../../Context/TaskContext/useTaskContext';
 
 export function History() {
+  const { state } = useTaskContext();
+
   return (
     <MainTemplate>
       <Container>
@@ -25,28 +28,30 @@ export function History() {
 
       <Container>
         <div className={styles.responsiveTable}>
-          <thead>
-            <tr>
-              <th>Tarefa</th>
-              <th>Duração</th>
-              <th>Data</th>
-              <th>Status</th>
-              <th>Tipo</th>
-            </tr>
-          </thead>
-          <tbody>
-            {Array.from({ length: 20 }).map((_, index) => {
-              return (
-                <tr key={index}>
-                  <td>Estudar</td>
-                  <td>25min</td>
-                  <td>20/04/2025</td>
-                  <td>Completa</td>
-                  <td>Foco</td>
-                </tr>
-              );
-            })}
-          </tbody>
+          <table>
+            <thead>
+              <tr>
+                <th>Tarefa</th>
+                <th>Duração</th>
+                <th>Data</th>
+                <th>Status</th>
+                <th>Tipo</th>
+              </tr>
+            </thead>
+            <tbody>
+              {state.tasks.map(task => {
+                return (
+                  <tr key={task.id}>
+                    <td>{task.name}</td>
+                    <td>{task.duration}min</td>
+                    <td>{new Date(task.startDate).toISOString()}</td>
+                    <td>{task.interruptDate}</td>
+                    <td>{task.type}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
         </div>
       </Container>
     </MainTemplate>
